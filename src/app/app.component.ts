75bd6da6150cef95b8383ff6a7ea43fa0e4abdc0
urielp <urielprb@gmail.com>
2018-01-15 13:53:10 +0200

    Adding edit option to app and improving edit component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,8 @@ export class AppComponent implements OnInit{
   public newTodo:ToDo=new ToDo();
 
   todolist:ToDo[];
+  editList:ToDo[] =[];
+  editmode:boolean=false;
 
   ngOnInit(){
     this.todoService.getToDos()
@@ -33,11 +35,54 @@ export class AppComponent implements OnInit{
       })
   }
 
+  submitTodo(event, todo:ToDo){
+    if(event.keyCode ==13){
+      this.editTodo(todo)
+    }
+  }
+
+  doneTodo(todo:ToDo){
+    todo.status = 'Done'
+    this.editmode=true;
+    this.todoService.editTodo(todo).subscribe(res => {
+      console.log('Update Succesful')
+    }, err => {
+      this.editTodo(todo)
+      console.error('Update Unsuccesful')
+      this.editmode=false;
+    })
+  }
+
   deleteTodo(todo:ToDo){
     this.todoService.deleteTodo(todo._id)
       .subscribe(res => {
       let index = this.todolist.indexOf(todo);
       this.todolist.splice(index,1);
     });
+  }
+
+  editTodo(todo:ToDo){
+
+    this.editmode=true;
+    if(this.todolist.includes(todo))
+    {
+      if(!this.editList.includes(todo)){
+        this.editList.push(todo);
+      }
+      else {
+        this.editmode=false;
+        let index = this.editList.indexOf(todo);
+        this.editList.splice(index,1);
+        this.todoService.editTodo(todo)
+          .subscribe(res=>{
+            console.log("updated");
+            this.editmode=false;
+          },err=>{
+            this.editmode=false;
+            this.editTodo(todo);
+            console.error("update failed");
+          })
+      }
+    }
   }
 }
