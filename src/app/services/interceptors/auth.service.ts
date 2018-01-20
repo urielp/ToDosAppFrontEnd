703e20520a8bc46a89c780afc2688b730fac8747
urielprb@gmail.com <065393188@Bb>
2018-01-20 13:59:44 +0200

    adding interceptor

---
+++ src/app/services/interceptors/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Injector } from '@angular/core';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/observable/throw'
+import 'rxjs/add/operator/catch';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    console.log("intercepted request ... ");
+
+// Clone the request to add the new header.
+   // const authReq = req.clone({ headers: req.headers.set("headerName","headerValue")});
+    const header = new HttpHeaders();
+    header.set('abc','22');
+    const authReq = req.clone({headers :header});
+
+    console.log("Sending request with new header now ...");
+    console.log(authReq.headers);
+
+//send the newly created request
+    return next.handle(authReq)
+      .catch((error, caught) => {
+//intercept the respons error and displace it to the console
+        console.log("Error Occurred");
+        console.log(error);
+//return the error to the method that called it
+        return Observable.throw(error);
+      }) as any;
+  }
+}
